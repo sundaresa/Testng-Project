6578aa1d30cd7afb47b113e18fe365d1c7848e7f
sundaresa <48744626+sundaresa@users.noreply.github.com>
2019-03-21 07:50:27 +0530

    Add files via upload

---
+++ ReportN.java
@@ -0,0 +1,21 @@
+package report;
+
+import org.testng.annotations.Test;
+
+public class ReportN {
+
+	
+@Test
+public void validLogin(){
+	System.out.println("valid");
+}
+@Test
+public void invalidLogin(){
+	System.out.println("Inavild");
+}
+
+}
+	
+	
+	
+
